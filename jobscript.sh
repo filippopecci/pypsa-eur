94d24ce5df2c56f8fa8f124e1129c9a792b17bd4
Filippo Pecci <peccifilippo@gmail.com>
2026-06-11 18:01:19 +0200

    generalize to europe beyond italy

--- jobscript.sh
+++ jobscript.sh
@@ -2,17 +2,20 @@
 #BSUB -o log_%J.out
 #BSUB -e log_%J.err
 #BSUB -q p_macro
-#BSUB -n 16
+#BSUB -n 32
 #BSUB -P 0588
 #BSUB -W 1440
 #BSUB -x
-#BSUB -R "span[ptile=16]"
+#BSUB -R "span[ptile=32]"
 #BSUB -R "rusage[mem=500G]"
 
+export TMPDIR=/work/cmcc/fp01525/tmp
+mkdir -p "$TMPDIR"
+export PATH="/users_home/cmcc/fp01525/.pixi/bin:$PATH"
+export PIXI_CACHE_DIR=/work/cmcc/fp01525/.cache/pixi
 export XDG_CACHE_HOME=/work/cmcc/fp01525/.cache
-export HDF5_USE_FILE_LOCKING=FALSE
-pixi shell
-snakemake -call \
+
+pixi run snakemake --cores 32\
     resources/pypsa-macro-italy/networks/base_s_adm___2025_brownfield.nc \
     resources/pypsa-macro-italy/networks/base_s_adm___2030.nc \
     resources/pypsa-macro-italy/networks/base_s_adm___2035.nc \
